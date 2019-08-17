4bbdc99900eb20321e2de9ab735312e0a8a54805
Trevor Bailey <tbailey@rscllcorp.com>
2019-08-17 07:54:07 -0400

    Initial submit

---
+++ app/src/main/java/kavorka/dndspelltracker/Character.java
@@ -0,0 +1,91 @@
+package kavorka.dndspelltracker;
+
+class Character {
+
+
+
+    private int mHitPoints;
+    private int mLevel;
+    private String mName;
+
+    // Stats
+    private int mStr;
+    private int mDex;
+    private int mCon;
+    private int mInt;
+    private int mWis;
+    private int mCha;
+
+    // Getters and setters
+    public int getHitPoints() {
+        return mHitPoints;
+    }
+
+    public void setHitPoints(int hitPoints) {
+        mHitPoints = hitPoints;
+    }
+
+    public int getLevel() {
+        return mLevel;
+    }
+
+    public void setLevel(int level) {
+        mLevel = level;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+    public int getStr() {
+        return mStr;
+    }
+
+    public void setStr(int str) {
+        mStr = str;
+    }
+
+    public int getDex() {
+        return mDex;
+    }
+
+    public void setDex(int dex) {
+        mDex = dex;
+    }
+
+    public int getCon() {
+        return mCon;
+    }
+
+    public void setCon(int con) {
+        mCon = con;
+    }
+
+    public int getInt() {
+        return mInt;
+    }
+
+    public void setInt(int anInt) {
+        mInt = anInt;
+    }
+
+    public int getWis() {
+        return mWis;
+    }
+
+    public void setWis(int wis) {
+        mWis = wis;
+    }
+
+    public int getCha() {
+        return mCha;
+    }
+
+    public void setCha(int cha) {
+        mCha = cha;
+    }
+}
