aae73a59158b76e828460fc5c4f95307d582bc74
Trevor Bailey <tbailey@rscllcorp.com>
2019-08-19 23:27:20 -0400

    - Update character class to have all 9 levels of spell slots  - Update Cleric class to set more spell slots

--- app/src/main/java/kavorka/dndspelltracker/CharacterClass.java
+++ app/src/main/java/kavorka/dndspelltracker/CharacterClass.java
@@ -5,6 +5,13 @@ public class CharacterClass {
 
     private int mLvl1SpellMax;
     private int mLvl2SpellMax;
+    private int mLvl3SpellMax;
+    private int mLvl4SpellMax;
+    private int mLvl5SpellMax;
+    private int mLvl6SpellMax;
+    private int mLvl7SpellMax;
+    private int mLvl8SpellMax;
+    private int mLvl9SpellMax;
 
 
     CharacterClass(Character character) {
@@ -13,6 +20,20 @@ public class CharacterClass {
 
     public void setSpells(Character character) {
     // Override this is each class
+    }
+
+    public int[] getSpellSlots() {
+        int[] slots = new int[9];
+        slots[0] = mLvl1SpellMax;
+        slots[1] = mLvl2SpellMax;
+        slots[2] = mLvl3SpellMax;
+        slots[3] = mLvl4SpellMax;
+        slots[4] = mLvl5SpellMax;
+        slots[5] = mLvl6SpellMax;
+        slots[6] = mLvl7SpellMax;
+        slots[7] = mLvl8SpellMax;
+        slots[8] = mLvl9SpellMax;
+        return slots;
     }
 
     public int getLvl1SpellMax() {
@@ -29,5 +50,61 @@ public class CharacterClass {
 
     public void setLvl2SpellMax(int mLvl2SpellMax) {
         this.mLvl2SpellMax = mLvl2SpellMax;
+    }
+
+    public int getLvl3SpellMax() {
+        return mLvl3SpellMax;
+    }
+
+    public void setLvl3SpellMax(int lvl3SpellMax) {
+        mLvl3SpellMax = lvl3SpellMax;
+    }
+
+    public int getLvl4SpellMax() {
+        return mLvl4SpellMax;
+    }
+
+    public void setLvl4SpellMax(int lvl4SpellMax) {
+        mLvl4SpellMax = lvl4SpellMax;
+    }
+
+    public int getLvl5SpellMax() {
+        return mLvl5SpellMax;
+    }
+
+    public void setLvl5SpellMax(int lvl5SpellMax) {
+        mLvl5SpellMax = lvl5SpellMax;
+    }
+
+    public int getLvl6SpellMax() {
+        return mLvl6SpellMax;
+    }
+
+    public void setLvl6SpellMax(int lvl6SpellMax) {
+        mLvl6SpellMax = lvl6SpellMax;
+    }
+
+    public int getLvl7SpellMax() {
+        return mLvl7SpellMax;
+    }
+
+    public void setLvl7SpellMax(int lvl7SpellMax) {
+        mLvl7SpellMax = lvl7SpellMax;
+    }
+
+    public int getLvl8SpellMax() {
+        return mLvl8SpellMax;
+    }
+
+    public void setLvl8SpellMax(int lvl8SpellMax) {
+        mLvl8SpellMax = lvl8SpellMax;
+    }
+
+    public int getLvl9SpellMax() {
+        return mLvl9SpellMax;
+    }
+
+    public void setLvl9SpellMax(int lvl9SpellMax) {
+        mLvl9SpellMax = lvl9SpellMax;
     }
 }
--- app/src/main/java/kavorka/dndspelltracker/Cleric.java
+++ app/src/main/java/kavorka/dndspelltracker/Cleric.java
@@ -15,6 +15,47 @@ public class Cleric extends CharacterClass {
         else if (charLvl == 3) {
             setLvl1SpellMax(4);
             setLvl2SpellMax(2);
+        } else if (charLvl == 4) {
+            setLvl1SpellMax(4);
+            setLvl2SpellMax(3);
+        } else if (charLvl == 5) {
+            setLvl1SpellMax(4);
+            setLvl2SpellMax(3);
+            setLvl3SpellMax(2);
+        } else if (charLvl == 6) {
+            setLvl1SpellMax(4);
+            setLvl2SpellMax(3);
+            setLvl3SpellMax(3);
+        } else if (charLvl == 7) {
+            setLvl1SpellMax(4);
+            setLvl2SpellMax(3);
+            setLvl3SpellMax(3);
+            setLvl4SpellMax(1);
+        } else if (charLvl == 8) {
+            setLvl1SpellMax(4);
+            setLvl2SpellMax(3);
+            setLvl3SpellMax(3);
+            setLvl4SpellMax(2);
+        } else if (charLvl == 9) {
+            setLvl1SpellMax(4);
+            setLvl2SpellMax(3);
+            setLvl3SpellMax(3);
+            setLvl4SpellMax(3);
+            setLvl5SpellMax(1);
+        } else if (charLvl == 10) {
+            setLvl1SpellMax(4);
+            setLvl2SpellMax(3);
+            setLvl3SpellMax(3);
+            setLvl4SpellMax(3);
+            setLvl5SpellMax(2);
+        } else if (charLvl == 11) {
+            setLvl1SpellMax(4);
+            setLvl2SpellMax(3);
+            setLvl3SpellMax(3);
+            setLvl4SpellMax(3);
+            setLvl5SpellMax(2);
+            setLvl6SpellMax(1);
         }
+
     }
 }
