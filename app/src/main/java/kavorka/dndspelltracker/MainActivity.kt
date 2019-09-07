5f950a291e2d48e3a19907cf18e74fbfca9286c5
Trevor Bailey <tbailey@rscllcorp.com>
2019-09-07 08:53:38 -0400

    - Test Git 3

--- app/src/main/java/kavorka/dndspelltracker/MainActivity.kt
+++ app/src/main/java/kavorka/dndspelltracker/MainActivity.kt
@@ -45,7 +45,7 @@ class MainActivity : AppCompatActivity() {
         // Clear the data!
 //        (applicationContext.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager).clearApplicationUserData()
 
-        // Test GIT 2
+        // Test GIT 3
 
         recyclerView = findViewById(R.id.charactersRecyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
