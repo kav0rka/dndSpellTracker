b31386d3c25f1df56d6f1b283db353e96ade90c3
Trevor Bailey <trevorbailey808@gmail.com>
2019-09-13 15:47:49 -0400

    - Added druid sub class abilities

--- app/src/main/java/kavorka/dndspelltracker/Druid.kt
+++ app/src/main/java/kavorka/dndspelltracker/Druid.kt
@@ -1,5 +1,6 @@
 package kavorka.dndspelltracker
 
+import kavorka.dndspelltracker.data.Ability
 import kavorka.dndspelltracker.data.PlayerCharacter
 
 // Sub classes
@@ -15,6 +16,16 @@ class Druid(playerCharacter: PlayerCharacter) : CharacterClass(playerCharacter) 
         if (level >= 2) {
             subClasses.add(land)
             subClasses.add(moon)
+
+            // Wild shape
+            abilities.add(Ability("", "Wild Shape", 2, resetOnShort = true))
+
+            // Sub class abilities
+            val sub = playerCharacter.characterSubClass
+
+            if (sub == land) {
+                abilities.add(Ability("", "Natural Recovery", 1))
+            }
         }
     }
 }
