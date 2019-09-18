b2c554235e6934e2bb22b61ed626546fdb8bcecb
Trevor Bailey <trevorbailey808@gmail.com>
2019-09-18 16:39:34 -0400

    - Cleanup warlock

--- app/src/main/java/kavorka/dndspelltracker/Warlock.kt
+++ app/src/main/java/kavorka/dndspelltracker/Warlock.kt
@@ -25,15 +25,15 @@ class Warlock(playerCharacter: PlayerCharacter) : CharacterClass(playerCharacter
             lvl1SpellMax = 1
         else if (level == 2)
             lvl1SpellMax = 2
-        else if (level == 3 || level == 4) {
+        else if (level in 3..4) {
             lvl2SpellMax = 2
-        } else if (level == 5 || level == 6) {
+        } else if (level in 5..6) {
             lvl3SpellMax = 2
-        } else if (level == 7 || level == 8) {
+        } else if (level in 7..8) {
             lvl4SpellMax = 2
-        } else if (level == 9 || level == 10) {
+        } else if (level in 9..10) {
             lvl5SpellMax = 2
-        } else if (level >= 11 && level <= 16) {
+        } else if (level in 11..16) {
             lvl5SpellMax = 3
         } else if (level >= 17) {
             lvl5SpellMax = 4
