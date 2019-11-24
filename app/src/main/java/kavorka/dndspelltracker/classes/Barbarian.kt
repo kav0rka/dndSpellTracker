b55afef7f51a476689471bdd6f3f6df8b85cc60a
Trevor <trevorbailey808@gmail.com>
2019-11-24 16:07:34 -0500

    Add XGE Barbarian sub classes

--- app/src/main/java/kavorka/dndspelltracker/classes/Barbarian.kt
+++ app/src/main/java/kavorka/dndspelltracker/classes/Barbarian.kt
@@ -5,6 +5,9 @@ import kavorka.dndspelltracker.data.PlayerCharacter
 // Sub classes
 const val berserker = "Berserker"
 const val totemWarrior = "Totem Warrior"
+const val ancestralGuardian = "Ancestral Guardian"
+const val stormHerald = "Storm Herald"
+const val zealot = "Zealot"
 
 class Barbarian(playerCharacter: PlayerCharacter) : CharacterClass(playerCharacter) {
 
@@ -15,6 +18,9 @@ class Barbarian(playerCharacter: PlayerCharacter) : CharacterClass(playerCharact
         if (level >= 3) {
             subClasses.add(berserker)
             subClasses.add(totemWarrior)
+            subClasses.add(ancestralGuardian)
+            subClasses.add(stormHerald)
+            subClasses.add(zealot)
         }
     }
 }
