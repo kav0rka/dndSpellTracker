7bbe5eda96a4401e488cb64eead720922d10e108
Trevor <trevorbailey808@gmail.com>
2019-12-07 16:54:43 -0500

    Update Bard with ZGE

--- app/src/main/java/kavorka/dndspelltracker/classes/Bard.kt
+++ app/src/main/java/kavorka/dndspelltracker/classes/Bard.kt
@@ -7,6 +7,9 @@ import kavorka.dndspelltracker.getAbilityMod
 // Sub classes
 const val lore = "Lore"
 const val valor = "Valor"
+const val glamour = "Glamour"
+const val swords = "Swords"
+const val whispers = "Whispers"
 
 class Bard(playerCharacter: PlayerCharacter) : CharacterClass(playerCharacter) {
 
@@ -21,6 +24,20 @@ class Bard(playerCharacter: PlayerCharacter) : CharacterClass(playerCharacter) {
         if (level >= 3) {
             subClasses.add(lore)
             subClasses.add(valor)
+            subClasses.add(glamour)
+            subClasses.add(swords)
+            subClasses.add(whispers)
+        }
+
+        val subClass = playerCharacter.characterSubClass
+        if (subClass == glamour) {
+            if (level >= 3) abilities.add(Ability("", "Enthralling Performance", 1, resetOnShort = true))
+            if (level >= 6) abilities.add(Ability("", "Mantle of Majesty", 1))
+            if (level >=14) abilities.add(Ability("", "Unbreakable Majesty", 1, resetOnShort = true))
+        } else if (subClass == whispers) {
+            if (level >= 3) abilities.add(Ability("", "Words of Terror", 1, resetOnShort = true))
+            if (level >= 6) abilities.add(Ability("", "Mantle of Whispers", 1, resetOnShort = true))
+            if (level >= 14) abilities.add(Ability("", "Shadow Lore", 1))
         }
     }
 }
