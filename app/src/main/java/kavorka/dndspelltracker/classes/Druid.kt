ac61f8fc8a53e8e00deb379e08e674dc036bce49
Trevor <trevorbailey808@gmail.com>
2019-12-07 17:15:21 -0500

    Update Druid with ZGE

--- app/src/main/java/kavorka/dndspelltracker/classes/Druid.kt
+++ app/src/main/java/kavorka/dndspelltracker/classes/Druid.kt
@@ -2,10 +2,13 @@ package kavorka.dndspelltracker.classes
 
 import kavorka.dndspelltracker.data.Ability
 import kavorka.dndspelltracker.data.PlayerCharacter
+import kavorka.dndspelltracker.getAbilityMod
 
 // Sub classes
 const val land = "Land"
 const val moon = "Moon"
+const val dreams = "Dreams"
+const val shepard = "Shepard"
 
 class Druid(playerCharacter: PlayerCharacter) : CharacterClass(playerCharacter) {
 
@@ -17,6 +20,8 @@ class Druid(playerCharacter: PlayerCharacter) : CharacterClass(playerCharacter) 
         if (level >= 2) {
             subClasses.add(land)
             subClasses.add(moon)
+            subClasses.add(dreams)
+            subClasses.add(shepard)
 
             // Wild shape
             abilities.add(Ability("", "Wild Shape", 2, resetOnShort = true))
@@ -26,6 +31,13 @@ class Druid(playerCharacter: PlayerCharacter) : CharacterClass(playerCharacter) 
 
             if (sub == land) {
                 abilities.add(Ability("", "Natural Recovery", 1))
+            } else if (sub == dreams) {
+                val wisdomMod = getAbilityMod(playerCharacter.wisdom)
+                if (level >= 2) abilities.add(Ability("", "Balm of the Summer Court", level))
+                if (level >= 6) abilities.add(Ability("", "Hidden Paths", wisdomMod))
+                if (level >= 14) abilities.add(Ability("", "Walker in Dreams", 1))
+            } else if (sub == shepard) {
+                if (level >= 14) abilities.add(Ability("", "Faithful Summons", 1))
             }
         }
     }
